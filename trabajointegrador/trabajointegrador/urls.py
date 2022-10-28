a036539b33afc7f4bf6ec098ea0f11113fb01fee
martiarena842 <tomimartiarena@gmail.com>
2022-10-28 10:34:29 -0300

    prueba templates

--- trabajointegrador/trabajointegrador/urls.py
+++ trabajointegrador/trabajointegrador/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from cgitb import html
 from django.contrib import admin
 from django.urls import path
+from el_lector import views as v
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('',v.index),
 ]
