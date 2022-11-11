1e06debc41ffad70629797e328078001a42716c8
martiarena842 <tomimartiarena@gmail.com>
2022-11-11 11:29:12 -0300

    testeando models

--- trabajointegrador/el_lector/models.py
+++ trabajointegrador/el_lector/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    bar_code=models.CharField("Codigo Barras",max_length=100)
+    description=models.CharField("Descripcion",max_length=100)
+    price=models.DecimalField("Precio",max_digits=5,decimal_places=2)
+    
