5dfaeca5de0c43cceb4c33a78f186bb49d7501f8
JulianRosas <julymaxrosas@gmail.com>
2022-11-28 20:44:59 -0300

    models done

--- trabajointegrador/el_lector/admin.py
+++ trabajointegrador/el_lector/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Product
-from django.contrib.auth.models import User
-
+from .models import Pais
+from .models import Tipo_documento
+from .models import Domicilio
+from .models import Genero
+from .models import Imprenta
+from .models import Persona
+from .models import Autor
+from .models import Libro
 # Register your models here.
-admin.site.register(Product)
+
+admin.site.register(Pais)  
+admin.site.register(Tipo_documento)
+admin.site.register(Domicilio)
+admin.site.register(Genero)
+admin.site.register(Imprenta)
+admin.site.register(Persona)
+admin.site.register(Autor)
+admin.site.register(Libro)
 
