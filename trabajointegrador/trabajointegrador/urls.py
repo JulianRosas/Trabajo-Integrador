be32b5759b0e392e414efc423d7d50ff632d7848
LautaroVelez <lautivelez28@gmail.com>
2022-12-01 23:05:46 -0300

    ready

--- trabajointegrador/trabajointegrador/urls.py
+++ trabajointegrador/trabajointegrador/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from el_lector import views as v
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', v.index_view, name="index"),
     path('login/', v.login, name='login'),
     path('addbook/', v.addbook_view, name='addbook_view'),
-    path('register/', v.register, name='register'),
     path('booklist/', v.booklist, name='booklist'),
     path('success/', v.addbook, name="addbook")
 
